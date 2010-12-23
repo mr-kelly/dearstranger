d357f24f811245b8005e9e201064cf9a69f10d0f
mrkelly <mrkelly@mrkelly-jolicloud.(none)>
2010-12-23 13:42:41 +0800

    a

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -79,6 +79,8 @@ $config['profile_constellation'] = array(
 */
 $config['base_url']	= "/xxmm/";
 
+$config['static_url'] = $config['base_url'] . 'static/';
+ 
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -89,7 +91,7 @@ $config['base_url']	= "/xxmm/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "";
+$config['index_page'] = "index.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -107,7 +109,7 @@ $config['index_page'] = "";
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= "PATH_INFO";
+$config['uri_protocol']	= "AUTO";
 
 /*
 |--------------------------------------------------------------------------
