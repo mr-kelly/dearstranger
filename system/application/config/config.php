7b5a2c4d78742f8d7182681abab3172a9c1cc5a9
mrkelly <mrkelly@mrkelly-jolicloud.(none)>
2010-12-25 15:27:39 +0800

    Change

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -109,7 +109,7 @@ $config['index_page'] = "index.php";
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= "AUTO";
+$config['uri_protocol']	= "PATH_INFO";
 
 /*
 |--------------------------------------------------------------------------
